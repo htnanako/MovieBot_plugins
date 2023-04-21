bd6b603923184baab4f6644175a9682e709795de
htnanako <794046218@qq.com>
2023-04-21 17:15:01 +0800

    add zhuque_game_file

---
+++ zhuque_game_fire/README.md
@@ -0,0 +1,16 @@
+## 朱雀站点技能批量释放
+
+> 基于MovieBot插件系统实现的朱雀站点技能批量释放。
+
+### 使用
+
+#### 一、获取网站cookie及x_csrf_token
+
+打开朱雀网站(地址自查)，登录后，按F12打开开发者工具，切换到网络选项卡，刷新页面，点击Fetch/XHR，从下方任选一个请求，右边标头选项卡里即可找到cookie及x_csrf_token。
+![zhuque](https://s2.loli.net/2023/04/21/FJm25kdWygQDAYh.png)
+
+
+#### 二、下载本插件，解压后拷贝到MovieBot插件目录
+
+
+#### 三、重启MovieBot，进入插件管理，配置刚刚获取的cookie及x_csrf_token
---
+++ zhuque_game_fire/event.py
@@ -0,0 +1,200 @@
+from mbot.core.plugins import plugin, PluginContext, PluginMeta, PluginCommandContext, PluginCommandResponse
+from mbot.openapi import mbot_api
+from typing import Dict, Any
+import logging
+import httpx
+import json
+import time
+import datetime
+import random
+from dateutil.parser import parse
+from threading import Thread
+
+server = mbot_api
+_LOGGER = logging.getLogger(__name__)
+
+url = 'https://zhuque.in/api/gaming/listGenshinCharacter'
+fire_url = 'https://zhuque.in/api/gaming/fireGenshinCharacterMagic'
+img_url = "https://s2.loli.net/2023/02/08/7IOmzLKbPZGDNC2.jpg"
+
+
+def async_call(fn):
+    def wrapper(*args, **kwargs):
+        Thread(target=fn, args=args, kwargs=kwargs).start()
+    return wrapper
+
+
+@async_call
+def start_main():
+    times, has_character = zhuque_character()
+    try:
+        now_time = parse(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        if now_time >= times:
+            fire()
+            start_main()
+        else:
+            delta = times - now_time
+            delta_second = delta.total_seconds()
+            if delta_second >= 3600:
+                hours = delta_second // 3600
+                remaining_seconds = delta_second % 3600
+                minutes = remaining_seconds // 60
+                seconds = remaining_seconds % 60
+                remain_time = f'{int(hours):02d}小时{int(minutes):02d}分钟{int(seconds):02d}秒'
+            elif delta_second >= 60:
+                minutes = delta_second // 60
+                seconds = delta_second % 60
+                remain_time = f'{int(minutes):02d}分钟{int(seconds):02d}秒'
+            else:
+                remain_time = f'{int(delta_second):02d}秒'
+            logging.info(f'[zhuque_game_file]:开始睡觉，{remain_time}后起来干活。')
+            time.sleep(int(delta_second) + random.randint(60, 120))
+            start_main()
+    except Exception as e:
+        logging.error(f'[zhuque_game_file]:释放技能出错了,错误信息：{e}', exc_info=True)
+        return
+
+
+
+@plugin.after_setup
+def after_setup(plugin_meta: PluginMeta, config: Dict[str, Any]):
+    global message_to_uid, channel, task_switch, cookie, x_csrf_token
+    message_to_uid = config.get('uid')
+    channel = config.get('ToChannelName')
+    task_switch = config.get('task_switch')
+    cookie = config.get('cookie')
+    x_csrf_token = config.get('x_csrf_token')
+    _LOGGER.info(f'[zhuque_game_file]:ZHUQUE释放技能插件初始化完成。')
+    start_main()
+
+
+@plugin.config_changed
+def config_changed(config: Dict[str, Any]):
+    _LOGGER.info('[zhuque_game_file]:ZHUQUE释放技能插件配置更新。')
+    global message_to_uid, channel, task_switch, cookie, x_csrf_token
+    message_to_uid = config.get('uid')
+    channel = config.get('ToChannelName')
+    task_switch = config.get('task_switch')
+    cookie = config.get('cookie')
+    x_csrf_token = config.get('x_csrf_token')
+    
+
+@plugin.task('zhuque_fire_task', '执行朱雀批量释放技能', cron_expression='0 0 * * *')
+def task():
+    if task_switch:
+        start_main()
+
+
+@plugin.command(name='zhuque_fire', title='释放技能', desc='朱雀站批量释放技能', icon='LocalFireDepartment', run_in_background=True)
+def zhuque_fire_echo(ctx: PluginCommandContext):
+    _LOGGER.info(f'开始获取角色信息')
+    times, has_character = zhuque_character()
+    try:
+        now_time = parse(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        if now_time >= times:
+            fire()
+            return PluginCommandResponse(True, f'释放技能成功')
+        else:
+            _LOGGER.info(f'[zhuque_game_file]:时间未到。')
+            return PluginCommandContext(True, f'时间未到。')
+    except Exception as e:
+        logging.error(f'[zhuque_game_file]:释放技能出错了,错误信息：{e}', exc_info=True)
+        return PluginCommandResponse(False, f'释放技能出错了,{e}')
+
+
+def zhuque_character():
+    has_character, times_list = [], []
+    headers = {
+        'cookie': cookie,
+        'x-csrf-token': x_csrf_token,
+    }
+    try:
+        response = httpx.get(url, headers=headers, timeout=30).text
+        response = json.loads(response)
+        for i in response['data']['characters']:
+            name = i['key']
+            if i['info']:
+                level = i['info']['level']
+                next_time = int(i['info']['next_time'])
+                next_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(next_time))
+                h = f'角色：{name}(Lv.{level})\n时间：{next_time}'
+                times = parse(next_time)
+                times_list.append(times)
+                has_character.append(h)
+        max_time = latest_time(times_list)
+        return max_time, has_character
+    except Exception as e:
+        logging.error(f'[zhuque_game_file]:获取角色信息出错了,错误信息{e}', exc_info=True)
+        return
+
+
+def latest_time(times_list):
+    today = datetime.date.today()
+    past_list = []
+    for i in times_list:
+        if i.date() <= today:
+            past_list.append(i)
+    if past_list:
+        return max(past_list)
+    else:
+        return datetime.datetime(9999, 12, 31, 23, 59, 59)
+
+
+def fire():
+    content = f''
+    headers = {
+        'accept': 'application/json, text/plain, */*',
+        'referer': "https://zhuque.in/gaming/genshin/character/list",
+        'x-requested-with': 'XMLHttpRequest',
+        'content-type': 'application/json',
+        'origin': 'https://zhuque.in',
+        'cookie': cookie,
+        'x-csrf-token': x_csrf_token,
+    }
+    data = {
+        'all': 1
+    }
+    try:
+        response = httpx.post(fire_url, headers=headers, json=data, timeout=30)
+        res = response.json()
+        if res.get('status') == 200:
+            _LOGGER.info(f'[zhuque_game_file]:已拥有角色：')
+            times, has_character = zhuque_character()
+            for h in has_character:
+                content += h + '\n'
+                _LOGGER.info(h)
+            bonus = res.get('data').get('bonus')
+            _LOGGER.info(f'[zhuque_game_file]:释放技能成功,获得了{bonus}灵石')
+            title = f'释放技能成功,获得了{bonus}灵石'
+            sent_notify(title, content)
+            return True
+        else:
+            _LOGGER.error(f'[zhuque_game_file]:释放技能失败，时间还未到')
+            return False
+    except Exception as e:
+        logging.error(f'[zhuque_game_file]:释放技能出错了,错误信息：{e}', exc_info=True)
+        return False
+
+
+def sent_notify(title, content):
+    if message_to_uid:
+        for _ in message_to_uid:
+            server.notify.send_message_by_tmpl('{{title}}', '{{a}}', {
+                'title': title,
+                'a': content,
+                'pic_url': img_url
+            }, to_uid=_, to_channel_name=channel)
+
+
+def main():
+    times, has_character = zhuque_character()
+    try:
+        now_time = parse(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        if now_time >= times:
+            fire()
+            return True
+        else:
+            return False
+    except Exception as e:
+        logging.error(f'[zhuque_game_file]:释放技能出错了,错误信息：{e}', exc_info=True)
+        return False
